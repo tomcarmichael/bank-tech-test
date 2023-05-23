9a0f60a66a44e8c7a25e39bae41c0b6535d628b8
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-23 12:52:46 +0100

    Moved feature tests to unit test file where they are only testing Account class functionality

--- account.test.js
+++ account.test.js
@@ -18,6 +18,21 @@ describe('Account class unit test', () => {
     const account = new Account(Balance);
     expect(account.balance.current).toEqual(0);
   });
+
+  it('allows the user to deposit 1000', () => {
+    account = new Account(Balance);
+    expect(account.deposit(1000)).toEqual('Deposit successful, resulting balance is 1000');
+  });
+
+  it('allows the user to deposit 500', () => {
+    account = new Account(Balance);
+    expect(account.deposit(500)).toEqual('Deposit successful, resulting balance is 500');
+  });
+
+  it("doesn't allow the user to deposit 0", () => {
+    account = new Account(Balance);
+    expect(account.deposit(0)).toEqual('Deposit unsuccessful, cannot deposit 0');
+  });
 });
 
 
