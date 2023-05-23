2072d2aa87fd3d250608e1e34f3cf2524a6937c0
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-23 16:53:43 +0100

    Ammend unit tests in Account to all use dependency injection for mock Transaction class

--- account.test.js
+++ account.test.js
@@ -26,31 +26,31 @@ describe('Account class unit test', () => {
 
   describe('constructor', () => {
     it('initializes with a balance of 0',() => {
-      const account = new Account(Balance);
+      const account = new Account(Balance, Transaction);
       expect(account.balance.current).toEqual(0);
     });
 
     it('initializes with an empty array of transcations',() => {
-      const account = new Account(Balance);
+      const account = new Account(Balance, Transaction);
       expect(account.transactions).toEqual([]);
     });
   });
 
   describe('deposit()', () => {
     it('allows the user to deposit 1000', () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       expect(account.deposit(1000)).toEqual('Deposit successful, resulting balance is 1000.00');
       expect(account.balance.current).toEqual(1000);
     });
   
     it('allows the user to deposit 500', () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       expect(account.deposit(500)).toEqual('Deposit successful, resulting balance is 500.00');
       expect(account.balance.current).toEqual(500);
     });
   
     it("doesn't allow the user to deposit 0", () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       expect(account.deposit(0)).toEqual('Deposit unsuccessful, cannot deposit 0');
       expect(account.balance.current).toEqual(0);
     });
@@ -66,26 +66,26 @@ describe('Account class unit test', () => {
 
   describe('withdraw()', () => {
     it("doesn't allow the user to withdraw more than their balance", () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       account.deposit(300);
       expect(account.balance.current).toEqual(300);
       expect(account.withdraw(500)).toEqual('Insufficient funds. Your current balance is 300.00');
     });
 
     it("doesn't allow the user to withdraw before they have made a deposit", () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       expect(account.withdraw(5)).toEqual('Insufficient funds. Your current balance is 0.00');
     });
 
     it("allows the user to withdraw some money and updates their balance", () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       account.deposit(5000);
       expect(account.withdraw(500)).toEqual('Withdrawl successful, resulting balance is 4500.00');
       expect(account.balance.current).toEqual(4500);
     });
 
     it("allows the user to withdraw 1000 and updates their balance", () => {
-      account = new Account(Balance);
+      account = new Account(Balance, Transaction);
       account.deposit(2000);
       expect(account.withdraw(1000)).toEqual('Withdrawl successful, resulting balance is 1000.00');
       expect(account.balance.current).toEqual(1000);
