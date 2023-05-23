a7d6c2f8b62555213afeb1792517dcf95a47cc6d
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-23 12:43:52 +0100

    Write and pass integration test for account initializing with balance of 0

--- integration.test.js
+++ integration.test.js
@@ -1,8 +1,14 @@
 const Account = require('./account');
+const Balance = require('./balance');
 
 describe('Bank account integration test', () => {
+  it('initializes with a balance of 0', () => {
+    account = new Account(Balance);
+    expect(account.balance.current).toEqual(0);
+  });
+
   it('allows the user to deposit money', () => {
-    account = new Account();
+    account = new Account(Balance);
     expect(account.deposit(1000)).toEqual('Deposit successful, resulting balance is £1000');
   });
 });
