e65a2984ee36421d2937f1b86cfb10e06f1d9ab9
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-23 14:04:03 +0100

    Refactor account unit tests to check balance after deposits

--- account.test.js
+++ account.test.js
@@ -22,16 +22,19 @@ describe('Account class unit test', () => {
   it('allows the user to deposit 1000', () => {
     account = new Account(Balance);
     expect(account.deposit(1000)).toEqual('Deposit successful, resulting balance is 1000.00');
+    expect(account.balance.current).toEqual(1000);
   });
 
   it('allows the user to deposit 500', () => {
     account = new Account(Balance);
     expect(account.deposit(500)).toEqual('Deposit successful, resulting balance is 500.00');
+    expect(account.balance.current).toEqual(500);
   });
 
   it("doesn't allow the user to deposit 0", () => {
     account = new Account(Balance);
     expect(account.deposit(0)).toEqual('Deposit unsuccessful, cannot deposit 0');
+    expect(account.balance.current).toEqual(0);
   });
 });
 
